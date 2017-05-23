6f7dc8411b5d1571a0edf398de19db84b152a22f
vitchulkov <vitchulkov1970@gmail.com>
2017-05-23 22:08:52 +0300

    tips

--- RestService2_03-04-2017/Controllers/LkController.cs
+++ RestService2_03-04-2017/Controllers/LkController.cs
@@ -89,6 +89,91 @@ namespace RestService.Controllers
            return null;
        }
 
+       [HttpGet]
+       public ActionResult TipsExport(string dfrom, string dto, int restaurant_id)
+       {
+           DateTime begindate = DateTime.Parse(dfrom);
+           begindate = (begindate.Year == 1) ? new DateTime(1900, 1, 1) : begindate;
+           DateTime enddate = DateTime.Parse(dto);
+           enddate = (enddate.Year == 1) ? DateTime.Today : enddate;
+           TimeSpan time = new TimeSpan(23, 59, 59);
+           enddate = enddate.Add(time);
+
+           try
+           {
+               var result = string.Empty;
+               var wb = new Workbook();
+               // properties
+               wb.Properties.Created = DateTime.Today;
+
+               // options sheets
+               wb.ExcelWorkbook.ActiveSheet = 1;
+               wb.ExcelWorkbook.DisplayInkNotes = false;
+               wb.ExcelWorkbook.FirstVisibleSheet = 1;
+               wb.ExcelWorkbook.ProtectStructure = false;
+               wb.ExcelWorkbook.WindowHeight = 800;
+               wb.ExcelWorkbook.WindowTopX = 0;
+               wb.ExcelWorkbook.WindowTopY = 0;
+               wb.ExcelWorkbook.WindowWidth = 600;
+
+               // create style s1 for header
+               var s1 = new Style("s1") { Font = new Font { Bold = true, Italic = true, Color = "#FF0000" } };
+               wb.AddStyle(s1);
+
+               // create style s2 for header
+               var s2 = new Style("s2") { Font = new Font { Bold = true, Italic = true, Size = 12, Color = "#0000FF" } };
+               wb.AddStyle(s2);
+
+               // First sheet
+               var ws = new Worksheet("Чаевые");
+
+               // Adding Headers
+               ws.AddCell(0, 0, "Дата заказа", 0);
+               ws.AddCell(0, 1, "Ресторан", 0);
+               ws.AddCell(0, 2, "Стол", 0);
+               ws.AddCell(0, 3, "Сумма заказа, руб.", 0);
+               ws.AddCell(0, 4, "Чаевые, %", 0);
+               ws.AddCell(0, 5, "Чаевые, руб.", 0);
+               ws.AddCell(0, 6, "Чаевые к выдаче, руб.", 0);
+               ws.AddCell(0, 7, "Официант", 0);
+
+               // get data
+               List<Tip> list = LkData.GetTipsList(begindate, enddate, restaurant_id);
+               if (list != null)
+               {
+                   int n = 0;
+                   for (int i = 0; i < list.Count; i++)
+                   {
+                       ws.AddCell(i + n + 1, 0, (list[i].PaymentDate.Year > 1970) ? (list[i].PaymentDate.ToShortDateString() + " " + list[i].PaymentDate.ToShortTimeString()) : "", 0);
+                       ws.AddCell(i + n + 1, 1, list[i].RestaurantName, 0);
+                       ws.AddCell(i + n + 1, 2, list[i].TableID, 0);
+                       ws.AddCell(i + n + 1, 3, list[i].OrderSum, 0);
+                       ws.AddCell(i + n + 1, 4, list[i].TippingProcent.ToString("0.00"), 0);
+                       ws.AddCell(i + n + 1, 5, list[i].TippingSum.ToString("0.00"), 0);
+                       decimal tips = Decimal.Round((list[i].TippingSum * 90) / 100);
+                       ws.AddCell(i + n + 1, 6, tips.ToString("0.00"), 0);
+                       ws.AddCell(i + n + 1, 7, list[i].WaiterName, 0);
+                   }
+                   wb.AddWorksheet(ws);
+
+                   // generate xml 
+                   var workstring = wb.ExportToXML();
+
+                   // Send to user file
+                   return new ExcelResult("Tipping.xls", workstring);
+               }
+           }
+           catch (Exception ex)
+           {
+               string error = ex.Message;
+           }
+
+           return null;
+       }
+
+
+
+
 
        [HttpGet]
        public ActionResult Report()
