2296c7e78443b4e9282e2126ae51334f79458a5e
vitchulkov <vitchulkov1970@gmail.com>
2017-06-05 23:54:18 +0300

    lk

--- RestService2_03-04-2017/Data/Lk/LkData.cs
+++ RestService2_03-04-2017/Data/Lk/LkData.cs
@@ -68,6 +68,119 @@ namespace RestService
             {
                 using (SqlConnection con = new SqlConnection(BLL.Configs.ConnectionString))
                 {
+                    SqlCommand cmd = new SqlCommand("Rest.dbo.GetLkAllPrize", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@dfrom", SqlDbType.DateTime).Value = dfrom;
+                    cmd.Parameters.Add("@dto", SqlDbType.DateTime).Value = dto;
+                    cmd.Parameters.Add("@restaurant_id", SqlDbType.Int).Value = restaurantID;
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Prize prize = new Prize();
+                        prize.PhoneNumber = (string)reader["phone_number"];
+                        if (reader["payment_date"] != DBNull.Value)
+                        {
+                            prize.PaymentDate = (DateTime)reader["payment_date"];
+                        }
+                        prize.PaymentID = (reader["id"] != DBNull.Value) ? Convert.ToInt32(reader["id"]) : 0;
+                        prize.RestaurantID = (reader["restaurantID"] != DBNull.Value) ? Convert.ToInt32(reader["restaurantID"]) : 0;
+                        prize.RestaurantName = (reader["name"] != DBNull.Value) ? Convert.ToString(reader["name"]) : "";
+                        prize.TableID = (reader["tableID"] != DBNull.Value) ? Convert.ToString(reader["tableID"]) : "";
+                        prize.WaiterID = (reader["waiterId"] != DBNull.Value) ? Convert.ToInt32(reader["waiterId"]) : 0;
+                        prize.WaiterName = (reader["waiterName"] != DBNull.Value) ? Convert.ToString(reader["waiterName"]) : "";
+                        if (prize != null && prize.PaymentID != 0 && CheckFirst(prize.PhoneNumber, prize.PaymentID))
+                        {
+                            list.Add(prize);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                string except = e.Message;
+            }
+
+            return list;
+        }
+
+        public static List<Prize> GetPrizeOtherList(DateTime dfrom, DateTime dto, int restaurantID)
+        {
+            List<Prize> list = new List<Prize>();
+            try
+            {
+                using (SqlConnection con = new SqlConnection(BLL.Configs.ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("Rest.dbo.GetLkAllPrize", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@dfrom", SqlDbType.DateTime).Value = dfrom;
+                    cmd.Parameters.Add("@dto", SqlDbType.DateTime).Value = dto;
+                    cmd.Parameters.Add("@restaurant_id", SqlDbType.Int).Value = restaurantID;
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Prize prize = new Prize();
+                        prize.PhoneNumber = (string)reader["phone_number"];
+                        if (reader["payment_date"] != DBNull.Value)
+                        {
+                            prize.PaymentDate = (DateTime)reader["payment_date"];
+                        }
+                        prize.PaymentID = (reader["id"] != DBNull.Value) ? Convert.ToInt32(reader["id"]) : 0;
+                        prize.RestaurantID = (reader["restaurantID"] != DBNull.Value) ? Convert.ToInt32(reader["restaurantID"]) : 0;
+                        prize.RestaurantName = (reader["name"] != DBNull.Value) ? Convert.ToString(reader["name"]) : "";
+                        prize.TableID = (reader["tableID"] != DBNull.Value) ? Convert.ToString(reader["tableID"]) : "";
+                        prize.WaiterID = (reader["waiterId"] != DBNull.Value) ? Convert.ToInt32(reader["waiterId"]) : 0;
+                        prize.WaiterName = (reader["waiterName"] != DBNull.Value) ? Convert.ToString(reader["waiterName"]) : "";
+                        if (prize != null && prize.PaymentID != 0 && !CheckFirst(prize.PhoneNumber, prize.PaymentID))
+                        {
+                            list.Add(prize);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                string except = e.Message;
+            }
+
+            return list;
+        }
+
+
+        private static bool CheckFirst(string phone_number, int payment_id)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(BLL.Configs.ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("Rest.dbo.GetLkFirstPrize", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@phone_number", SqlDbType.VarChar).Value = phone_number;
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        int id = (reader["id"] != DBNull.Value) ? (int)reader["id"] : 0;
+                        if (id == payment_id) { return true; }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                string except = e.Message;
+            }
+            return false;
+        }
+
+        /*
+        public static List<Prize> GetPrizeList(DateTime dfrom, DateTime dto, int restaurantID)
+        {
+            List<Prize> list = new List<Prize>();
+            try
+            {
+                using (SqlConnection con = new SqlConnection(BLL.Configs.ConnectionString))
+                {
                     SqlCommand cmd = new SqlCommand("Rest.dbo.GetLkPrize", con);
                     cmd.CommandType = CommandType.StoredProcedure;
                     cmd.Parameters.Add("@dfrom", SqlDbType.DateTime).Value = dfrom;
@@ -145,7 +258,7 @@ namespace RestService
 
             return list;
         }
-
+        */
 
     }
 }
